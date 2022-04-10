313bbf333a13667fcf55d0bf47ef40b41e93e788
Faysal Mahmud <kishorepasha347@gmail.com>
2022-04-11 00:46:00 +0600

    Add solution for problem `A - Young Physicist` Link: https://codeforces.com/contest/69/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/69/submission/153325307

---
+++ codeforces/69/A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ; cin >> n ;
+    int arr[n][3];
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            cin >> arr[i][j] ;
+        }
+    }   
+
+    int x=0 , y=0 , z=0 ;
+
+    for(int i = 0 ; i < n ; i++){
+        x+= arr[i][0];
+        y+= arr[i][1];
+        z+= arr[i][2];
+    } 
+    // cout << arr[2][2] << " " << y << " " << z ;
+    if(x == 0 && y == 0 && z == 0) cout << "YES";
+    else cout << "NO";
+
+}

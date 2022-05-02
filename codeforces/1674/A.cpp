15de885303f1226aa4a51738cef280fe4900bdc4
Faysal Mahmud <kishorepasha347@gmail.com>
2022-05-02 21:04:00 +0600

    Add solution for problem `A - Number Transformation` Link: https://codeforces.com/contest/1674/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1674/submission/155626194

--- codeforces/1674/A.cpp
+++ codeforces/1674/A.cpp
@@ -9,9 +9,27 @@ int main(){
     while(t--){
         int x , y ; 
         cin >> x >> y ; 
-        if(y % x == 0){
-            cout << "1 " << y/x << "\n" ;
+        if(x > y) cout << "0 0\n" ; 
+        else if(x == y) cout << "3 1\n" ;
+        else {
+            if(y % x != 0) cout <<  "0 0\n" ; 
+            else {
+                int p = y / x ;
+                int b , a; 
+                for(int i = 2 ; ;i++){
+                    b = i ;
+                    a = lab(p , b) ;
+                    if(floor(lab(p , i)) == ceil(lab(p , i))){ 
+                        cout << a << " " << b << "\n"; 
+                        break ;
+                    }
+                    else if(pow(b , a) > p){
+                        cout << "0 0\n" ;
+                        break ;
+                    } 
+                }
+                
+            }
         }
-        else cout << "0 0\n" ;
     }
 }

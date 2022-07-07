07caf569ff1f11d4e64772592b19fed969b75f26
Faysal Mahmud <kishorepasha347@gmail.com>
2022-07-07 15:28:00 +0600

    Add solution for problem `A - Letter` Link: https://codeforces.com/contest/14/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/14/submission/163088415

--- codeforces/14/A.cpp
+++ codeforces/14/A.cpp
@@ -4,28 +4,51 @@ int main(){
     int n , m ; 
     cin >> n >> m ;
     char arr[n][m] ;
+    vector<int> row , col ;
     for(int i = 0 ; i < n ; i++){
+        int r = 0 ;
         for(int j = 0 ; j < m ; j++){
             cin >> arr[i][j] ;
-        }
-    }
-
-
-    int row_high = n-1 , col_high = m-1 , row_low = 0 , col_low = 0 ;
-
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < m ; j++){
-            if(arr[i][j] == '*'){
-                row_low = max(row_low , i) ;
-                col_low = max(col_low , j) ;
-                row_high = min(row_high , i) ;
-                col_high = min(col_high , j) ;
+            if(arr[i][j] == '.'){
+                r++ ;
             }
         }
+        if(r == m) row.push_back(i);
+        else row.push_back(-1) ;
     }
 
-    // cout << row_high <<" "<< col_high <<" "<< row_low <<" "<< col_low <<"\n" ;
-   
+    for(int i = 0 ; i < m ; i++){
+        int c = 0 ;
+        for(int j = 0 ; j < n ; j++){
+            if(arr[j][i] == '.'){
+                c++ ;
+            }
+        }
+        if(c == n) col.push_back(i);
+        else col.push_back(-1) ;
+    }
+
+    // for(int i = 0 ; i < col.size() ; i++) cout << col[i] << " " ;
+    int row_high = 0 , col_high = 0 , row_low = n-1 , col_low = m-1 ;
+
+    for(int i = 0 ; i < row.size() ; i++){
+        if(row[i] == -1) break ;
+        else row_high = i+1 ;
+    }
+    for(int i = row.size()-1 ; i >= 0 ; i--){
+        if(row[i] == -1) break ;
+        else row_low = i-1 ;
+    }
+    for(int i = 0 ; i < col.size() ; i++){
+        if(col[i] == -1) break ;
+        else col_high = i+1 ;
+    }
+    for(int i = col.size()-1 ; i >= 0 ; i--){
+        if(col[i] == -1) break ;
+        else col_low = i-1;
+    }
+
+    // cout << row_high << " " << row_low << " " << col_high << " " << col_low << "\n" ;
 
     for(int i = row_high ; i <= row_low ; i++){
         for(int j = col_high ; j <= col_low ; j++){
